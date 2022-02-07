34d57ef2c801b3cb73da493c25309c21bca20338
yash gupta <yash3144@gmail.com>
2022-02-07 23:45:09 +0530

    props in card component and add css to them

--- components/productCard/card.js
+++ components/productCard/card.js
@@ -16,21 +16,42 @@ const Card = (props) => {
 //     small: styles.smItem,
 //   };
 
+  console.log("props:",props)
   const handleOnError = () => {
     console.log("Some error")
     setImgSrc('https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
   };
 
-  const scale = id === 0 ? { scaleY: 1.1 } : { scale : 1.1};
+//   const scale = id === 0 ? { scaleY: 1.1 } : { scale : 1.1};
 
   return (
     <div className={styles.container} >
       {/* <div className={cls(styles.imgMotionWrapper,classMap[size])} whileHover={{...scale}}> */}
-      <div className={styles.imgMotionWrapper} >
-        <Image src={props.image} alt="" layout="fill" className={styles.cardImg} 
-        onError={handleOnError}
-        />
+      <div className={styles.headComponent}>
+
+        <div className={styles.imgMotionWrapper} >
+            <img src={props.productDetails.image} alt="" layout="fill" className={styles.cardImg} 
+            onError={handleOnError} width="200" height="100"
+            />
+        </div>
+
+        <div className={styles.productNameBrand}>
+            <h4 className={styles.productNameElement}>{props.productDetails.product_name}</h4>
+            <h4 className={styles.productBrandElement}>{props.productDetails.brand_name}</h4>
+            <p className={styles.costElement}>$ {props.productDetails.price}</p>
+        </div>
+
       </div>
+
+      <div className={styles.middleComponent}>
+
+      </div>
+
+      <div className={styles.bottomComponent}>
+
+      </div>
+
+      
     </div>
   );
 };
--- components/productCard/productCard.js
+++ components/productCard/productCard.js
@@ -3,18 +3,18 @@ import styles from "./ProductCard.module.css"
 import Card from "./card";
 
 const ProductCard = (props) => {
-    console.log({props});
+    // console.log({props});
     const { title, products=[]} = props;
-    console.log({ products });
+    // console.log({ products });
     return (
         <section className={styles.container}>
-        <h2 className={styles.title}>{title}</h2>
-        
-        <div className={styles.cardWrapper}>
-            {products.map((product, idx) => {
-            return <Card id={idx} productDetails={product}  />;
-            })}
-        </div>
+            <h2 className={styles.title}>{title}</h2>
+            
+            <div className={styles.cardWrapper}>
+                {products.map((product, idx) => {
+                return <Card key={idx} productDetails={product}  />;
+                })}
+            </div>
         </section>
     );
 };
