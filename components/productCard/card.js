899602f268ec77e6c629cb482d70c2f3dc901699
yash gupta <yash3144@gmail.com>
2022-02-08 12:22:36 +0530

    install moment package and adjust css for date

--- components/productCard/card.js
+++ components/productCard/card.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import styles from "./card.module.css";
 
 import cls from "classnames";
+import moment from "moment";
 // import { motion } from "framer-motion";
 
 const Card = (props) => {
@@ -17,10 +18,10 @@ const Card = (props) => {
 //   };
 
   console.log("props:",props)
-  const handleOnError = () => {
-    console.log("Some error")
-    setImgSrc('https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
-  };
+  // const handleOnError = () => {
+  //   console.log("Some error")
+  //   setImgSrc('https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80');
+  // };
 
 //   const scale = id === 0 ? { scaleY: 1.1 } : { scale : 1.1};
 
@@ -30,8 +31,7 @@ const Card = (props) => {
       <div className={styles.headComponent}>
 
         <div className={styles.imgMotionWrapper} >
-            <img src={props.productDetails.image} alt="" layout="fill" className={styles.cardImg} 
-            onError={handleOnError} 
+            <img src={props.productDetails.image} alt="" layout="fill" className={styles.cardImg}  
             />
         </div>
 
@@ -45,7 +45,7 @@ const Card = (props) => {
 
       <div className={styles.middleComponent}>
         <h4 className={styles.productNameElement}>{props.productDetails.address.state}, {props.productDetails.address.city}</h4>
-        <h4 className={styles.productBrandElement}>{props.productDetails.date}</h4>
+        <h4 className={styles.productDate}>{moment(props.productDetails.date).format('hh:mm:ss')}</h4>
       </div>
 
       <div className={styles.bottomComponent}>
